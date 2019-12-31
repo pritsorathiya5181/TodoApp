c50df33b006cd0b50ec9394eaefa685743af60ab
prit <prit.sorathiya@gmail.com>
2019-12-31 12:26:30 +0530

    Compelte app

---
+++ App.js
@@ -0,0 +1,110 @@
+import React from 'react';
+import { Platform, StyleSheet, Text, View, FlatList } from 'react-native';
+import Header from './components/Header'
+import InputBar from './components/InputBar'
+import TodoItem from './components/TodoItem'
+
+// export default function App() {
+//   return (
+//     <View style={styles.container}>
+//       <Text>Hello world!</Text>
+//     </View>
+//   );
+// }
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      todoInput: '',
+      todos: [
+        { id: 0, title: 'Take out the trash', done: false },
+        { id: 1, title: 'Cook dinner', done: false }
+
+      ]
+    }
+  }
+
+  addNewTodo() {
+
+    let todos = this.state.todos;
+    let title = this.state.todoInput;
+
+    if (title == '') { }
+    else {
+      todos.unshift({
+        id: todos.length + 1,
+        title,
+        done: false
+      });
+    }
+
+    this.setState({
+      todos: todos,
+      todoInput: ''
+    });
+  }
+
+  toggleDone(item) {
+    let todos = this.state.todos;
+
+    todos = todos.map((todo) => {
+      if (todo.id == item.id) {
+        todo.done = !todo.done;
+      }
+
+      return todo;
+    })
+
+    this.setState({ todos });
+  }
+
+  removeTodo(item) {
+    let todos = this.state.todos;
+
+    todos = todos.filter((todo) => todo.id !== item.id);
+
+    this.setState({ todos });
+  }
+
+  render() {
+    const statusbar = (Platform.OS == 'android') ? <View style={styles.statusbar}></View> : <View></View>;
+    return (
+      <View style={styles.container}>
+        {statusbar}
+
+        <Header title="Todoapp" />
+
+        <InputBar
+          addNewTodo={() => this.addNewTodo()}
+          textChange={todoInput => this.setState({ todoInput })}
+          todoInput={this.state.todoInput}
+        />
+
+        <FlatList
+          data={this.state.todos}
+          extraData={this.state}
+          keyExtractor={(item, index) => index.toString()}
+          renderItem={({ item, index }) => {
+            return (
+              <TodoItem todoItem={item} toggleDone={() => this.toggleDone(item)} removeTodo={() => this.removeTodo(item)} />
+            )
+          }}
+        />
+      </View>
+    );
+  }
+}
+
+export default App;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+  statusbar: {
+    backgroundColor: '#FFCE00',
+    height: 25
+  }
+});
